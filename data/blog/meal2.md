9d7b2fdd967b822b22042d50e3a82b273e7ddd8d
Annie <a0900610201@gmail.com>
2025-05-05 20:44:22 +0800

    上傳meal1 喫尤鐵板燒

--- data/blog/meal2.md
+++ data/blog/meal2.md
@@ -1,10 +1,12 @@
 ---
 title: 'Annie'
-date: '2018-08-15'
-tags: ['meals', 'dessert', 'reflection']
+date: '2024-08-15'
+tags: ['meals']
 draft: false
 summary: 美食
 type: Blog
 ---
 
 # 我是楊珮榆
+
+這個好吃呦
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -8,12 +8,11 @@ interface Project {
 const projectsData: Project[] = [
   {
     title: 'Experices',
-    description: `2022-2023                                              
-    Vice president and Activity Manager of ZhongShan English Debate Club
-    2025 
-    Drama director and Instructor of 2025 NTU Comsa Camp
-    2025<br>
-    Member of Public Relations Department of NTU IM night`,
+    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+    to the desired date and press "Go". No more worrying about lost keys or
+    forgotten headphones with this simple yet affordable solution.`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: '/blog/the-time-machine',
   },
   {
     title: 'The Time Machine',
